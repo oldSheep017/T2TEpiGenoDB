26de5006b43ccc18dc3b02405a11fc4bd3f329a0
yangchunrui <youngcoding@163.com>
2023-06-02 19:51:50 +0800

    config
    some select option

---
+++ src/utils/options.js
@@ -0,0 +1,33 @@
+const options = {
+  chromosome: [
+    'Chr 1',
+    'Chr 2',
+    'Chr 3',
+    'Chr 4',
+    'Chr 5',
+    'Chr 6',
+    'Chr 7',
+    'Chr 8',
+    'Chr 9',
+    'Chr 10',
+    'Chr 11',
+    'Chr 12',
+    'Chr 13',
+    'Chr 14',
+    'Chr 15',
+    'Chr 16',
+    'Chr 17',
+    'Chr 18',
+    'Chr 19',
+    'Chr 20',
+    'Chr 21',
+    'Chr 22',
+    'Chr X',
+    'Chr Y',
+    'Chr MT'
+  ]
+}
+
+export {
+  options
+}
