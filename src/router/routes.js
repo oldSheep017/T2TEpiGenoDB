d9bd625a9fc9bd07b2419d8df2df9cb3ac01b014
yangchunrui <youngcoding@163.com>
2023-06-02 19:50:10 +0800

    feat
    import VueRouter

---
+++ src/router/routes.js
@@ -0,0 +1,48 @@
+import Layout from '../layout/index.vue'
+import Home from '../views/home'
+import Search from '../views/search'
+import Browse from '../views/browse'
+import Download from '../views/download'
+import Contact from '../views/contact'
+import Help from '../views/help'
+
+const routes = [
+	{
+		path: '/',
+		component: Layout,
+		children: [
+			{
+				path: '',
+				name: 'Home',
+				component: Home,
+			},
+			{
+				path: 'search',
+				name: 'Search',
+				component: Search
+			},
+			{
+				path: 'browse',
+				name: 'Browse',
+				component: Browse
+			},
+			{
+				path: 'download',
+				name: 'Download',
+				component: Download
+			},
+			{
+				path: 'contact',
+				name: 'Contact',
+				component: Contact
+			},
+			{
+				path: 'help',
+				name: 'Help',
+				component: Help
+			},
+		],
+	},
+]
+
+export default routes
