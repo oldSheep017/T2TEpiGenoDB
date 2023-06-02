1ed799940bfb646dcee937840ad6e1d30a257855
yangchunrui <youngcoding@163.com>
2023-06-02 19:46:17 +0800

    feat
    contact page

---
+++ src/views/contact/index.js
@@ -0,0 +1,2 @@
+import Contact from './index.vue'
+export default Contact
---
+++ src/views/contact/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+Contact
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
