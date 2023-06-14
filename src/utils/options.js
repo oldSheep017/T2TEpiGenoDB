ceac11e2f038e9a7de11ecc7338f93679063161c
yangchunrui <youngcoding@163.com>
2023-06-15 02:22:56 +0800

    enhancement
    enhancing the chromosome select options.

--- src/utils/options.js
+++ src/utils/options.js
@@ -1,33 +1,106 @@
 const options = {
-  chromosome: [
-    'Chr 1',
-    'Chr 2',
-    'Chr 3',
-    'Chr 4',
-    'Chr 5',
-    'Chr 6',
-    'Chr 7',
-    'Chr 8',
-    'Chr 9',
-    'Chr 10',
-    'Chr 11',
-    'Chr 12',
-    'Chr 13',
-    'Chr 14',
-    'Chr 15',
-    'Chr 16',
-    'Chr 17',
-    'Chr 18',
-    'Chr 19',
-    'Chr 20',
-    'Chr 21',
-    'Chr 22',
-    'Chr X',
-    'Chr Y',
-    'Chr MT'
-  ]
+	chromosome: [
+		{
+			label: 'Chr 1',
+			value: 'Chr1',
+		},
+		{
+			label: 'Chr 2',
+			value: 'Chr2',
+		},
+		{
+			label: 'Chr 3',
+			value: 'Chr3',
+		},
+		{
+			label: 'Chr 4',
+			value: 'Chr4',
+		},
+		{
+			label: 'Chr 5',
+			value: 'Chr5',
+		},
+		{
+			label: 'Chr 6',
+			value: 'Chr6',
+		},
+		{
+			label: 'Chr 7',
+			value: 'Chr7',
+		},
+		{
+			label: 'Chr 8',
+			value: 'Chr8',
+		},
+		{
+			label: 'Chr 9',
+			value: 'Chr9',
+		},
+		{
+			label: 'Chr 10',
+			value: 'Chr10',
+		},
+		{
+			label: 'Chr 11',
+			value: 'Chr11',
+		},
+		{
+			label: 'Chr 12',
+			value: 'Chr12',
+		},
+		{
+			label: 'Chr 13',
+			value: 'Chr13',
+		},
+		{
+			label: 'Chr 14',
+			value: 'Chr14',
+		},
+		{
+			label: 'Chr 15',
+			value: 'Chr15',
+		},
+		{
+			label: 'Chr 16',
+			value: 'Chr16',
+		},
+		{
+			label: 'Chr 17',
+			value: 'Chr17',
+		},
+		{
+			label: 'Chr 18',
+			value: 'Chr18',
+		},
+		{
+			label: 'Chr 19',
+			value: 'Chr19',
+		},
+		{
+			label: 'Chr 20',
+			value: 'Chr20',
+		},
+		{
+			label: 'Chr 21',
+			value: 'Chr21',
+		},
+		{
+			label: 'Chr 22',
+			value: 'Chr22',
+		},
+		{
+			label: 'Chr X',
+			value: 'ChrX',
+		},
+		{
+			label: 'Chr Y',
+			value: 'ChrY',
+		},
+		{
+			label: 'Chr MT',
+			value: 'ChrMT',
+		},
+	],
 }
 
-export {
-  options
-}
+export { options }
