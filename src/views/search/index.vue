82dcf1774546746e5c15c4effc456a4cb0e1ad39
yangchunrui <youngcoding@163.com>
2023-06-15 02:24:07 +0800

    feat
    accomplishing the function 'search by gene locus'.

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -5,7 +5,7 @@
 			<div class="title" @click="changeCollapse" data-index="0">
 				Search by Gene locus
 			</div>
-			<main class="w-full h-[500px] bg-gray-200 grid grid-cols-2">
+			<main class="w-full h-[500px] grid grid-cols-2 border-x border-[#2e5e1c]">
 				<!-- Form -->
 				<section class="h-full box-border px-8">
 					<div class="grid grid-rows-[7]">
@@ -13,14 +13,10 @@
 						<div class="m-4 text-lg font-semibold">Chromosome</div>
 						<!-- chromosome select -->
 						<div>
-							<select
-								name=""
-								id=""
-								class="h-12 w-full box-border px-4 rounded-md border border-gray-500"
-								v-model="geneLocus.chromosome"
-							>
+							<select name="" id="" class="h-12 w-full box-border px-4 rounded-md border border-gray-500"
+								v-model="geneLocus.chromosome">
 								<template v-for="op in options.chromosome">
-									<option :value="op">{{ op }}</option>
+									<option :value="op.value">{{ op.label }}</option>
 								</template>
 							</select>
 						</div>
@@ -30,12 +26,8 @@
 						</div>
 						<!-- start position input -->
 						<div>
-							<input
-								class="h-12 w-full box-border px-4 rounded-md border border-gray-500"
-								placeholder="start position    e.g.: 47648539"
-								type="text"
-								v-model="geneLocus.start"
-							/>
+							<input class="h-12 w-full box-border px-4 rounded-md border border-gray-500"
+								placeholder="start position    e.g.: 47648539" type="text" v-model="geneLocus.start" />
 						</div>
 						<!-- end position label -->
 						<div class="m-4 text-lg font-semibold">
@@ -43,18 +35,14 @@
 						</div>
 						<!-- end position input -->
 						<div>
-							<input
-								class="h-12 w-full box-border px-4 rounded-md border border-gray-500"
-								placeholder="end position    e.g.: 4865523"
-								type="text"
-								v-model="geneLocus.end"
-							/>
+							<input class="h-12 w-full box-border px-4 rounded-md border border-gray-500"
+								placeholder="end position    e.g.: 4865523" type="text" v-model="geneLocus.end" />
 						</div>
 						<!-- button area -->
-						<div class="flex justify-end items-center my-3">
-							<button class="search-button">Start analysis</button>
-							<button class="search-button">Reset</button>
-							<button class="search-button">For Example</button>
+						<div class="flex md:flex-row flex-col justify-end items-center my-3 w-full">
+							<button class="search-button md:w-auto md:my-0 my-1 w-full" @click="GENELOCUS.query">Start analysis</button>
+							<button class="search-button md:w-auto md:my-0 my-1 w-full" @click="GENELOCUS.reset">Reset</button>
+							<button class="search-button md:w-auto md:my-0 my-1 w-full" @click="GENELOCUS.example">For Example</button>
 						</div>
 					</div>
 				</section>
@@ -75,7 +63,7 @@
 			<div class="title" @click="changeCollapse" data-index="1">
 				Search by Gene
 			</div>
-			<main class="w-full h-[500px] bg-gray-200 flex">
+			<main class="w-full h-[500px] border-x border-[#2e5e1c] flex">
 				<section></section>
 			</main>
 		</nav>
@@ -83,7 +71,7 @@
 			<div class="title" @click="changeCollapse" data-index="2">
 				Search by Sequence
 			</div>
-			<main class="w-full h-[500px] bg-gray-200"></main>
+			<main class="w-full h-[500px] border-x border-[#2e5e1c]"></main>
 		</nav>
 	</div>
 </template>
@@ -91,6 +79,8 @@
 <script setup>
 import { ref, reactive } from 'vue'
 import { options } from '../../utils'
+import { useRouter } from 'vue-router'
+const router = useRouter()
 const active = ref(0)
 const activeClass = ref(['active', '', ''])
 const changeCollapse = e => {
@@ -107,37 +97,60 @@ const changeCollapse = e => {
 
 // Form Data
 const geneLocus = reactive({
-	chromosome: 'Chr 1',
+	chromosome: 'Chr1',
 	start: '',
 	end: '',
 })
 
 const gene = reactive({})
 const sequence = reactive({})
+
+// Methods
+const GENELOCUS = {
+	query() {
+		// http.get(`/gene/locus?chromosome=${geneLocus.chromosome}&start=${geneLocus.start}&end=${geneLocus.end}`).then(res => {
+		// 	console.log('genelocus search result: ', res)
+		// })
+		router.push({ name: 'Analysis', query: { ...geneLocus } })
+	},
+	reset() {
+		geneLocus.chromosome = 'Chr1'
+		geneLocus.start = '0'
+		geneLocus.end = '0'
+	},
+	example() {
+		geneLocus.chromosome = 'Chr1'
+		geneLocus.start = '125494410'
+		geneLocus.end = '247971178'
+	}
+}
 </script>
 
 <style scoped>
 .main-title {
 	@apply text-5xl mx-10 my-4 transition-all w-[6ch] h-12;
 }
+
 .main-title:hover {
 	cursor: default;
 	filter: drop-shadow(0 0 16px #1e85d9);
 }
+
 .collapse-item {
 	transition: all 0.6s cubic-bezier(0.19, 1, 0.22, 1);
 	@apply w-full max-h-20 overflow-hidden box-border border-y border-gray-300;
 }
+
 nav.active {
 	max-height: calc(4rem + 500px);
 }
+
 .title {
-	@apply w-full h-20 bg-[#3a454b] text-white cursor-pointer flex items-center box-border px-6 text-2xl;
+	@apply w-full h-20 bg-[#2e5e1c] text-white cursor-pointer flex items-center box-border px-6 text-2xl;
 }
 
-
 .search-button {
-	@apply bg-[#3a454b] text-white p-2 rounded-md flex justify-center items-center active:bg-orange-500;
+	@apply bg-[#2e5e1c] text-white p-2 rounded-md flex justify-center items-center active:bg-orange-500;
 }
 
 .search-button:nth-child(2) {
