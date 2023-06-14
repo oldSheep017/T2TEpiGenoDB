6729edcc33c846df0b65d5ef05a93b01d5922512
yangchunrui <youngcoding@163.com>
2023-06-15 02:19:25 +0800

    style
    import new font.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import VueRouter from './router'
+import './styles/font.css'
 import './styles/index.css'
 
-createApp(App).use(VueRouter).mount('#app')
+createApp(App)
+.use(VueRouter)
+.mount('#app')
