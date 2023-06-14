9603d5656d5afabacfe13a9f6242921d5879cc78
yangchunrui <youngcoding@163.com>
2023-06-15 02:23:13 +0800

    style
    using new font.

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -1,10 +1,10 @@
 <template>
 	<header
 		class="h-16 shrink-0 w-full grow-0 bg-white box-border border-b border-light-600 flex justify-between items-cente px-8 md:px-20 text-xl">
-		<div class="flex items-center transition-all hover:rotate-[-3deg]">
+		<div class="flex items-center">
 			<img src="../../public/logo.svg" class="w-12 h-12" />
 			<div class="flex flex-col mx-2">
-				<span class="text-3xl text-gray-600 font-bold drop-shadow-lg font-sans cursor-default">T2TEpiGenoDB</span>
+				<span class="text-3xl text-gray-600 font-bold drop-shadow-lg font-brussels cursor-default">T2TEpiGenoDB</span>
 				<!-- <span class="text-base text-gray-600 draop-shadow-lg cursor-default">A database</span> -->
 			</div>
 		</div>
