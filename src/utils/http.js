41f0926c0ea9099c93da90d550fffe19417f9990
yangchunrui <youngcoding@163.com>
2023-06-17 21:23:05 +0800

    config
    config the 2 cases' baseURL
    - development
    - production

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
+const baseURL = {
+	DEVELOPMENT: 'http://localhost:3001',
+	PRODUCTION: 'https://www.youngcr.cn/api/t2t',
+}
 const instance = axios.create({
-  baseURL: 'http://localhost:3001',
-  timeout: 10 * 1000
+	baseURL: baseURL.PRODUCTION,
+	timeout: 10 * 1000,
 })
 
 export default instance
