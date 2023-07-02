3d654f6b8b76b7a7b38f39420dc988d785647e9d
yangchunrui <youngcoding@163.com>
2023-07-02 13:45:19 +0800

    style

--- src/components/Pagination/index.vue
+++ src/components/Pagination/index.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="flex h-10 w-max box-border border border-gray-400 my-2">
-    <div class="flex justify-center items-center text-sm border-r border-gray-400 px-2" @click="prev">Previous</div>
+    <div class="flex justify-center items-center text-sm border-r border-gray-400 px-2 cursor-pointer transition-colors hover:bg-[#165f0b] hover:text-white" @click="prev">Previous</div>
     <template v-for="i in 6">
-      <div class="border-r border-gray-400 h-full w-9 flex justify-center items-center">{{ i }}</div>
+      <div class="border-r border-gray-400 h-full w-9 flex justify-center items-center cursor-pointer transition-colors hover:bg-[#165f0b] hover:text-white" @click="changePage(i)">{{ i }}</div>
     </template>
-    <div class="flex justify-center items-center text-sm px-2" @click="next">Next</div>
+    <div class="flex justify-center items-center text-sm px-2 cursor-pointer transition-colors hover:bg-[#165f0b] hover:text-white" @click="next">Next</div>
   </section>
 </template>
 
@@ -33,7 +33,9 @@ const prev = () => {
   emit('change', page - 1)
 }
 
-const changePage = () => {}
+const changePage = (page) => {
+  emit('change', page)
+}
 
 const next = () => {
   if (page.value === count.value) {
