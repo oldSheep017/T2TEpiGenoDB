7e7d8d5dfe7a0028703c0328a0189e011f893634
yangchunrui <youngcoding@163.com>
2023-07-09 00:26:04 +0800

    feat: import jbrowse

--- src/views/browse/index.vue
+++ src/views/browse/index.vue
@@ -1,7 +1,9 @@
 <template>
+  <iframe :src="src" class="w-full h-[800px]" frameborder="0"></iframe>
 </template>
 
 <script setup>
+const src = 'https://jbrowse.org/code/jb2/main/?config=test_data%2Fvolvox%2Fconfig.json&session=local-1aJ03Hlc_M'
 </script>
 
 <style scoped></style>
