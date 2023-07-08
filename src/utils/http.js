d28c29f79562ac00994bf93e50c3328c3cf93397
yangchunrui <youngcoding@163.com>
2023-07-09 00:22:12 +0800

    refactor: change the baseURL

--- src/utils/http.js
+++ src/utils/http.js
@@ -4,8 +4,8 @@ const baseURL = {
 	PRODUCTION: 'https://www.youngcr.cn/api/t2t',
 }
 const instance = axios.create({
-	baseURL: baseURL.DEVELOPMENT,
-	// baseURL: baseURL.PRODUCTION,
+	// baseURL: baseURL.DEVELOPMENT,
+	baseURL: baseURL.PRODUCTION,
 	timeout: 10 * 1000,
 })
 
